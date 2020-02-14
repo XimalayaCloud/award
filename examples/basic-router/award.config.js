6dffb0702097049ead860850a7beead8c61f30fc
topthinking <topthinking@ximalaya.com>
2020-02-14 12:24:51 +0800

    修复单元测试错误

--- examples/basic-router/award.config.js
+++ examples/basic-router/award.config.js
@@ -1,12 +1,11 @@
 export default {
-  router: 'hash',
-  mode: 'client',
-  assetPrefixs: './',
-  hashName: false,
   // basename: '/test/a',
   fetch: {
     domainMap: {
       '/api': 'http://127.0.0.1:1234'
+    },
+    apiGateway: {
+      API_RETRY_TIME: 2
     }
   }
 };
