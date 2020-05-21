abb69ee8d31a172c8bed0af81c981d0850f99d1c
topthinking <topthinking@ximalaya.com>
2020-05-21 11:36:49 +0800

    调整路由和无路由项目的Head渲染逻辑区分

--- packages/award/src/head.tsx
+++ packages/award/src/head.tsx
@@ -4,9 +4,19 @@ import loadParams from 'award-utils/loadParams';
 
 export default (props: any) => {
   const { children, ...rests } = props;
-  const { ssr, isSwitchRouter } = loadParams.get();
-  if (ssr && !isSwitchRouter) {
-    return null;
+  const { ssr, isSwitchRouter, firstRender, routes } = loadParams.get();
+  if (ssr) {
+    if (routes.length) {
+      // 当前页面存在路由
+      if (!isSwitchRouter) {
+        return null;
+      }
+    } else {
+      // 当前页面不存在路由
+      if (firstRender) {
+        return null;
+      }
+    }
   }
   return (
     <>
