e481f85687678aa636ccb55361b3c15e46215f9d
topthinking <topthinking@ximalaya.com>
2021-01-25 15:45:22 +0800

    scope

--- packages/award-scripts/src/tools/style/postcss-parse/postcss-selector.ts
+++ packages/award-scripts/src/tools/style/postcss-parse/postcss-selector.ts
@@ -1,50 +1,56 @@
+/* eslint-disable max-nested-callbacks */
 /* eslint-disable array-callback-return */
 import * as postcss from 'postcss';
 
-export default postcss.plugin('postcss-selector', (styleId) => {
-  let uniqueInfo = `.${styleId}`;
+export default postcss.plugin(
+  'postcss-selector',
+  (opt: { styleId: string; scopePosition: 'tail' | 'head' }) => {
+    const { styleId, scopePosition } = opt;
+    let uniqueInfo = `.${styleId}`;
 
-  return (root: any) => {
-    root.walkRules((rule: any) => {
-      // 忽略keyframs内的过渡选择器
-      if (rule.parent.type === 'atrule') {
-        if (rule.parent.name === 'keyframes') {
-          return;
+    return (root: any) => {
+      root.walkRules((rule: any) => {
+        // 忽略keyframs内的过渡选择器
+        if (rule.parent.type === 'atrule') {
+          if (rule.parent.name === 'keyframes') {
+            return;
+          }
         }
-      }
 
-      const selectors = rule.selector.split(',');
+        const selectors = rule.selector.split(',');
 
-      selectors.map((item: any, index: any) => {
-        // 格式化选择器，存在伪类
-        if (/:(:)?/.test(item)) {
-          // 判断是否是a,b这样的选择器
-          // 存在伪类,针对最后一个进行处理
-          const _selector = item.split(' ');
+        selectors.map((item: any, index: any) => {
+          const selector = item.split(' ').filter((item: string) => item !== '');
+          let scopeSelector =
+            scopePosition === 'tail' ? selector[selector.length - 1] : selector[0];
 
-          let _last = _selector[_selector.length - 1];
-          const _match = _last.match(/(:(:)?.*)/);
+          // 格式化选择器，存在伪类
+          if (/:(:)?/.test(scopeSelector)) {
+            const match = scopeSelector.match(/(:(:)?.*)/);
 
-          if (_match !== null) {
-            // 最后一个选择器是伪类
-            _last = _last.replace(_match[0], `${uniqueInfo}${_match[0]}`);
-
-            _selector[_selector.length - 1] = _last;
-
-            item = _selector.join(' ');
+            if (match !== null) {
+              // 最后一个选择器是伪类
+              scopeSelector = scopeSelector.replace(match[0], `${uniqueInfo}${match[0]}`);
+            } else {
+              scopeSelector = scopeSelector + uniqueInfo;
+            }
           } else {
-            item = item + uniqueInfo;
+            if (['body', 'html'].indexOf(scopeSelector) === -1) {
+              scopeSelector = scopeSelector + uniqueInfo;
+            }
           }
-        } else {
-          if (['body', 'html'].indexOf(item.trim()) === -1) {
-            item = item + uniqueInfo;
-          }
-        }
 
-        selectors[index] = item;
+          if (scopePosition === 'tail') {
+            selector[selector.length - 1] = scopeSelector;
+          } else {
+            selector[0] = scopeSelector;
+          }
+
+          selectors[index] = selector.join(' ');
+        });
+
+        rule.selector = selectors.join(',');
       });
-
-      rule.selector = selectors.join(',');
-    });
-  };
-});
+    };
+  }
+);
--- packages/award-scripts/src/tools/style/utils/config.ts
+++ packages/award-scripts/src/tools/style/utils/config.ts
@@ -1,17 +1,35 @@
 import * as path from 'path';
 import * as fs from 'fs-extra';
 
-const defaultConfig = {
+const defaultConfig: Config = {
   plugins: [],
   limit: null,
-  randomScope: false
+  randomScope: false,
+  scopePosition: 'tail'
 };
 
-export default () => {
+export interface Config {
+  plugins: any[];
+  limit?: null;
+  randomScope?: boolean;
+  scopePosition: 'tail' | 'head';
+}
+
+export default (): Config => {
   const esConfigPath = path.join(process.cwd(), '.es-style.json');
-  let userConfig = {};
+  let userConfig: Config = {
+    plugins: [],
+    scopePosition: 'tail'
+  };
   if (fs.existsSync(esConfigPath)) {
     userConfig = JSON.parse(fs.readFileSync(esConfigPath, 'utf-8'));
+  }
+
+  if (userConfig.scopePosition !== 'tail' && userConfig.scopePosition !== 'head') {
+    userConfig.scopePosition = 'tail';
+  }
+  if (typeof userConfig.randomScope !== 'boolean') {
+    userConfig.randomScope = false;
   }
   return { ...defaultConfig, ...userConfig };
 };
